9f489ce2d850db01dbbf96a6ae3682289c9e386c
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-01-09 23:38:55 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HeroSection from './component/HeroSection';
+import About from './component/About';
+import NavBar from './component/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+        <NavBar />
         <Routes>
-           <Route path="/heroSection" element={ <HeroSection /> } />
+           <Route path="/" exact element={ <HeroSection />} />
+           <Route path="/about" exact element={ <About /> } />
         </Routes>
     </BrowserRouter>
   );
