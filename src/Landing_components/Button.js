0bed9394a8b550717c14eb1210d55d74d246125f
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-01-13 23:11:14 +0600

    made changes to the portfolio section

--- src/Landing_components/Button.js
+++ src/Landing_components/Button.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import style from '../Landing_components/button.module.css'
 
-const Button = ({changePage, buttonText, display, margin}) => {
+const Button = ({changePage, buttonText, display, margin, type, value}) => {
   return (
-    <button style={{display: display, margin: margin}} className={style.readMore} role="button" onClick={changePage}>{buttonText}</button>
+    <button type={type} value={value}  style={{display: display, margin: margin}} className={style.readMore} role="button" onClick={changePage}>{buttonText}</button>
   )
 }
 
--- src/Landing_components/ContactMe.js
+++ src/Landing_components/ContactMe.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 import style from '../Landing_components/contactMe.module.css'
 import Button from './Button'
 
 const ContactMe = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_qpz6shv', 'template_qcvw9v9', form.current, 'qG5Y8efyq_TOUSS_Z')
+        .then((result) => {
+        }, (error) => {
+        });
+
+        setName('')
+        setEmail('')
+        setMessage('')
+        };
+
   return (
     <div className={style.contactContainer}>
         <div className={style.heading}>
@@ -10,14 +29,14 @@ const ContactMe = () => {
            <h1>Contact Me</h1>
         </div>
 
-        <form className={style.form}>
+        <form className={style.form} ref={form} onSubmit={sendEmail} >
             <div className={style.formControl}>
-                <input type="text" placeholder="Your Name" />
-                <input type="email" placeholder="Your Email" />
+                <input type="text" value={name} onChange={(e)=> setName(e.target.value)} name="to_name" placeholder="Your Name" required/>
+                <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} name="from_name" placeholder="Your Email" required/>
             </div>
-            <textarea />
+            <textarea name="message" value={message} onChange={(e)=> setMessage(e.target.value)} required/>
             {
-                <Button buttonText="Send Message" display="block" margin="20px auto" />
+                <Button type="submit" value="Send" buttonText="Send Message" display="block" margin="20px auto" />
             }
         </form>
     </div>
